45925e290c0880fa8670a5d5d36b6a8174ae47cc
0xb1b1 <voxelflee@protonmail.com>
2023-11-09 23:44:30 +0300

    fix: make /users/all endpoint work correctly
    fix: controllers and models

--- iwcd-lct2023-backend/src/main/java/com/incelswithchronicdepression/lct2023/iwcdlct2023backend/controller/RequestController.java
+++ iwcd-lct2023-backend/src/main/java/com/incelswithchronicdepression/lct2023/iwcdlct2023backend/controller/RequestController.java
@@ -1,7 +1,5 @@
 package com.incelswithchronicdepression.lct2023.iwcdlct2023backend.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,27 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.incelswithchronicdepression.lct2023.iwcdlct2023backend.model.User;
+import com.incelswithchronicdepression.lct2023.iwcdlct2023backend.repo.RequestRepo;
 import com.incelswithchronicdepression.lct2023.iwcdlct2023backend.repo.UserRepo;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-public class UserController {
+public class RequestController {
 
     @Autowired
-    UserRepo repo;
+    RequestRepo repo;
 
-    Logger log = LoggerFactory.getLogger(UserController.class);
+    Logger log = LoggerFactory.getLogger(RequestController.class);
 
-    @GetMapping("/users/all")
-    public List<User> getAllUsers() {
-        return repo.findAll();
-    }
-
-    @PostMapping("/users/add")
-    public void addUser(@RequestBody User user) {
-        log.info("Added user {}", user);
-        repo.save(user);
-    }
+    // Accept `page` query parameter
+    // @GetMapping("/requests/list")
+    // public 
 }
--- iwcd-lct2023-backend/src/main/java/com/incelswithchronicdepression/lct2023/iwcdlct2023backend/model/UserAccess.java
+++ iwcd-lct2023-backend/src/main/java/com/incelswithchronicdepression/lct2023/iwcdlct2023backend/model/UserAccess.java
@@ -13,6 +13,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 // import jakarta.persistence.JoinColumn;
 // import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -24,9 +26,9 @@ import lombok.Data;
 public class UserAccess {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Integer id;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
 
     @Column(name = "access")
     @ColumnDefault("false")
